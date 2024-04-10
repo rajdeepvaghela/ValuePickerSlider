1360ee96a5f60a60cc27b9f91a4826250a24f713
rajdeepvaghela <rajdeep.vaghela610@gmail.com>
2024-04-10 18:55:43 +0530

    changing java version from 11 to 17

--- slider/build.gradle.kts
+++ slider/build.gradle.kts
@@ -27,11 +27,11 @@ android {
         kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 }
 
